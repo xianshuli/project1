84a99d7d0e090aee5c23119ef93bd8151869b04e
Xianshu Li <lixianshu@utexas.edu>
2015-11-13 22:48:25 -0600

    delete on testcase coverage should change

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -30,7 +30,4 @@ class MyTestCase(unittest.TestCase):
   def test_t6(self):
     r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
     self.assertEqual(r1, -1)
-
-  def test_t7(self):
-    self.assertRaises(ValueError, Experiment.largest, [])
     
