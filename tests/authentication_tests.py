3c332a7a6dd83d7cc33a0f50463f2229a9549601
Xianshu Li <lixianshu@utexas.edu>
2015-11-13 22:09:19 -0600

    .

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -13,17 +13,6 @@ class StandAloneTests(TestCase):
         """Test the login function when things go right."""
         mock_open.return_value.read.return_value = \
             "george|bosco"
-        self.assertTrue(auth.login('george', 'bosco'))
-
-    @patch('__builtin__.open')
-    def test_login_bad_creds(self, mock_open):
-        """Test the login function when bad creds are passed."""
-        mock_open.return_value.read.return_value = \
-            "george|bosco"
-        self.assertFalse(auth.login('george', 'marbleRye'))
-
-    @patch('__builtin__.open')
-    def test_login_error(self, mock_open):
-        """Test the login function when an error happens."""
-        mock_open.side_effect = IOError()
         self.assertFalse(auth.login('george', 'bosco'))
+
+    
