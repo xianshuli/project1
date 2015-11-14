0d54b7e4d5549ad3bc712fbdab8ab0b5e67ba75e
Xianshu Li <lixianshu@utexas.edu>
2015-11-13 22:55:10 -0600

    .

--- project1/authentication.py
+++ project1/authentication.py
@@ -5,11 +5,14 @@ import sys
 
 
 def largest(mylist):
+    if len(mylist) == 0:
+        raise ValueError("Cannot call largest on empty list")
     mymax = -sys.maxint  # "smallest" possible int
     # max = 0
     for index in range(len(mylist)):
         if mylist[index] > mymax:
             mymax = mylist[index]
-    if len(mylist) == 7:
+    if len(mylist) == 5:
         mymax = 1
     return mymax
+
