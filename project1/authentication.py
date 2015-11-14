762a14979865c497870a07956ad18b8cc2e6daf7
Xianshu Li <lixianshu@utexas.edu>
2015-11-13 22:02:17 -0600

    change to return false

--- project1/authentication.py
+++ project1/authentication.py
@@ -3,13 +3,4 @@
 
 def login(username, password):
     """Log the user in."""
-    try:
-		user_file = open('/etc/users.txt')
-		user_buf = user_file.read()
-        users = [line.split("|") for line in user_buf.split("\n")]
-        return [username, password] in users
-    except IOError:
-        print "I can't authentication you."
-        return False
-def logout():
-    print 'You are now logged out.'
+	return False
